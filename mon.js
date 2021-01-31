52930804da6e97cde68e8d8a4cae1a28fbd358b4
norahmad95 <artistnor95@gmail.com>
2021-01-31 17:05:12 +0200

    monalis

---
+++ mon.js
@@ -0,0 +1,18 @@
+var whatknow = prompt('more about Monalisa or Davinci?')
+
+var details
+
+if (whatknow = 'Monalisa') {
+    details = 'the painting probably of itlian noblewoman Lisa Ghocondo';
+} else if (whatknow = 'Davinci') {
+    details = 'he was italian, while his fam initally rested on his achievments as a pinter. he also bcamee know for his notebooks.';
+}
+
+function myFunction() {
+    alert("more to know");
+}
+myFunction();
+
+
+document.write(details);
+document.getElementById("details").innerText = details;
